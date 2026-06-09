3ea4013ff4a8d2062254eeec6cff4fbee87d29d1
delee5695 <delee5695@naver.com>
2026-06-09 11:43:07 -0400

    Update draw_segments_on_image function to optionally include points drawing

--- coordinate_extraction/draw_figures.py
+++ coordinate_extraction/draw_figures.py
@@ -4,8 +4,7 @@ import numpy as np
 import cv2
 
 
-def draw_segments_on_image(image, lines=None, circles=None):
-
+def draw_segments_on_image(image, lines=None, circles=None, points=None):
     # Create a copy to draw detected lines for visualization
     line_img = image.copy()
 
@@ -14,12 +13,10 @@ def draw_segments_on_image(image, lines=None, circles=None):
             coords = line[0] if len(line.shape) > 1 else line
             x1, y1, x2, y2 = map(int, coords)
 
-            # Generate a random BGR color for each distinct line segment
-            # We cap the lower bound at 50 so the colors aren't too dark to see on a black background
             random_color = (
-                random.randint(50, 255),  # Blue channel
-                random.randint(50, 255),  # Green channel
-                random.randint(50, 255),  # Red channel
+                random.randint(50, 255),  # Blue
+                random.randint(50, 255),  # Green
+                random.randint(50, 255),  # Red
             )
 
             # Draw the line segment with its unique color
@@ -35,34 +32,29 @@ def draw_segments_on_image(image, lines=None, circles=None):
             # Draw the circle's perimeter
             cv2.circle(line_img, center, radius, (255, 0, 0), 2)
 
-    # cv2.line(line_img, (0, 0), (10, 0), random_color, 5)
-    # cv2.line(line_img, (0, 30), (100, 30), random_color, 5)
-    # cv2.line(line_img, (0, 60), (500, 60), random_color, 5)
+    if points is not None:
+        for pt in points:
+            px, py = map(int, pt)
+            cv2.circle(line_img, (px, py), 8, (0, 0, 255), -1)
 
-    # Display right inside your notebook
     plt.imshow(cv2.cvtColor(line_img, cv2.COLOR_BGR2RGB))
     plt.title(
-        f"Detected Segments (lines: {len(lines) if lines is not None else 0}, circles: {len(circles[0]) if circles is not None else 0})"
+        f"Detected Segments (lines: {len(lines) if lines is not None else 0}, circles: {len(circles[0]) if circles is not None else 0}, points: {len(points) if points is not None else 0} )"
     )
     plt.axis("off")
     plt.show()
 
 
-def plot_points(points):
-    # Separate X and Y coordinates for plotting
+def plot_points(points, title="No Title Specified"):
     x_coords = [p[0] for p in points]
     y_coords = [p[1] for p in points]
 
-    # Clear any active plots
     plt.clf()
-
-    # Plot just the points using a scatter plot
     plt.scatter(x_coords, y_coords, color="blue", s=80, zorder=5)
-
     plt.axis("equal")
 
     # Format the plot area
-    plt.title("Extracted Geometric Points")
+    plt.title(title)
     plt.xlabel("X Coordinate")
     plt.ylabel("Y Coordinate")
     plt.gca().invert_yaxis()  # Invert Y-axis for image coordinates
